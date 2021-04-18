bb60e3636fc090d274e582d855d2fee4e4b01b74
Kisalaya Kishan <kislay.kishan007@gmail.com>
2021-04-19 02:52:53 +0530

    BgColor
    Bg color updated

--- JS/chart.js
+++ JS/chart.js
@@ -27,6 +27,11 @@ async function getcoviddata(){
                 data: yaxis,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
                    
                 ],
                 borderColor: [

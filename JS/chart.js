ad53fa7d43950a62f8db59c984b5100a59605db0
Kisalaya Kishan <kislay.kishan007@gmail.com>
2021-04-20 16:25:16 +0530

    Added Daily Decesased Chart

--- JS/chart.js
+++ JS/chart.js
@@ -10,6 +10,7 @@ async function getcoviddata(){
     var xaxis=[];
     var yaxis=[];
     var dailyrecoverd=[];
+    var deaths=[];
     for(i=(xlen-31);i<(xlen);i++)
     {
 
@@ -21,7 +22,8 @@ async function getcoviddata(){
         var day = vals[2];
         var newdate = vals[2]+'-'+vals[1];
         xaxis.push(newdate);
-        dailyrecoverd.push(graphdata.cases_time_series[i].dailyrecovered);       
+        dailyrecoverd.push(graphdata.cases_time_series[i].dailyrecovered); 
+        deaths.push(graphdata.cases_time_series[i].dailydeceased);      
         // xaxis.push(graphdata.cases_time_series[i].dateymd);
     
     }
@@ -72,6 +74,42 @@ async function getcoviddata(){
                 borderWidth: 1
             }
         
+        ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    var ctx = document.getElementById('myChart2').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: xaxis,
+            datasets: [
+            {
+                label: 'Daily Decesased',
+                data: deaths,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                   
+                ],
+                borderColor: [
+                    // 'rgba(255, 99, 132, 1)',
+                    // 'rgba(54, 162, 235, 1)',
+                    // 'rgba(255, 206, 86, 1)',
+                    'rgba(255, 7, 58,1)'
+                    // 'rgba(75, 192, 192, 1)',
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }
         ]
         },
         options: {

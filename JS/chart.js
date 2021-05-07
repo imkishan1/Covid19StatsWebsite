89872b26449e5ca48258c54b66f73d23509c2394
Kisalaya Kishan <kislay.kishan007@gmail.com>
2021-05-07 16:19:29 +0530

    changed the graph and color

--- JS/chart.js
+++ JS/chart.js
@@ -76,21 +76,21 @@ async function getcoviddata(){
                 ],
                 borderWidth: 1
             },
-            {
-                label: 'Daily Recovered Cases',
-                data: dailyrecoverd,
-                backgroundColor: [
-                    // 'rgba(255, 99, 132, 0.2)',
-                    'rgba(114, 194, 23, 0.2)'
+            // {
+            //     label: 'Daily Recovered Cases',
+            //     data: dailyrecoverd,
+            //     backgroundColor: [
+            //         // 'rgba(255, 99, 132, 0.2)',
+            //         'rgba(114, 194, 23, 0.2)'
                    
-                ],
-                borderColor: [
-                    // 'rgba(75, 192, 192, 1)',
-                    'rgba(114, 194, 23, 1)',
+            //     ],
+            //     borderColor: [
+            //         // 'rgba(75, 192, 192, 1)',
+            //         'rgba(114, 194, 23, 1)',
                     
-                ],
-                borderWidth: 1
-            }
+            //     ],
+            //     borderWidth: 1
+            // }
         
         ]
         },
@@ -147,42 +147,42 @@ async function getcoviddata(){
         }
     });
 
-    // var ctx = document.getElementById('myChart3').getContext('2d');
-    // var myChart = new Chart(ctx, {
-    //     type: 'bar',
-    //     data: {
-    //         // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    //         labels: xaxis,
-    //         datasets: [
-    //         {
-    //             label: 'Daily Recovered Cases',
-    //             data: dailyrecoverd,
-    //             backgroundColor: [
-    //                 // 'rgba(255, 99, 132, 0.2)',
-    //                 'rgba(114, 194, 23, 0.2)'
+    var ctx = document.getElementById('myChart3').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: xaxis,
+            datasets: [
+            {
+                label: 'Daily Recovered Cases',
+                data: dailyrecoverd,
+                backgroundColor: [
+                    // 'rgba(255, 99, 132, 0.2)',
+                    'rgba(114, 194, 23, 0.2)'
                    
-    //             ],
-    //             borderColor: [
-    //                 // 'rgba(255, 99, 132, 1)',
-    //                 // 'rgba(54, 162, 235, 1)',
-    //                 // 'rgba(255, 206, 86, 1)',
-    //                 'rgba(114, 194, 23, 1)',
-    //                 // 'rgba(75, 192, 192, 1)',
-    //                 // 'rgba(153, 102, 255, 1)',
-    //                 // 'rgba(255, 159, 64, 1)'
-    //             ],
-    //             borderWidth: 1
-    //         }
-    //     ]
-    //     },
-    //     options: {
-    //         scales: {
-    //             y: {
-    //                 beginAtZero: true
-    //             }
-    //         }
-    //     }
-    // });
+                ],
+                borderColor: [
+                    // 'rgba(255, 99, 132, 1)',
+                    // 'rgba(54, 162, 235, 1)',
+                    // 'rgba(255, 206, 86, 1)',
+                    'rgba(114, 194, 23, 1)',
+                    // 'rgba(75, 192, 192, 1)',
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }
+        ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 
  
 

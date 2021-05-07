07a60df7cd9ac70b9a06f0544660219c0dda9b47
Kisalaya Kishan <kislay.kishan007@gmail.com>
2021-05-07 16:54:54 +0530

    chart delted

--- JS/chart.js
+++ JS/chart.js
@@ -3,7 +3,7 @@ async function getcoviddata(){
     const url ='https://api.covid19india.org/data.json';
     const jsonyeardata = await fetch(url);
     const graphdata = await jsonyeardata.json();
-
+    
     const xlen = Object.keys(graphdata.cases_time_series).length;
     var i=0;
  
@@ -184,6 +184,42 @@ async function getcoviddata(){
         }
     });
 
+    var ctx = document.getElementById('myChart4').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: xaxis,
+            datasets: [
+            {
+                label: 'Daily Recovered Cases',
+                data: dailyrecoverd,
+                backgroundColor: [
+                    // 'rgba(255, 99, 132, 0.2)',
+                    'rgba(114, 194, 23, 0.2)'
+                   
+                ],
+                borderColor: [
+                    // 'rgba(255, 99, 132, 1)',
+                    // 'rgba(54, 162, 235, 1)',
+                    // 'rgba(255, 206, 86, 1)',
+                    'rgba(114, 194, 23, 1)',
+                    // 'rgba(75, 192, 192, 1)',
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }
+        ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
  
 
 }

b75c4de37397ef75a3337796a9b7d180a8e03971
Kisalaya Kishan <kislay.kishan007@gmail.com>
2021-05-13 19:31:54 +0530

    Vaccination data added

--- JS/gridchart.js
+++ JS/gridchart.js
@@ -4,6 +4,24 @@ async function getcovidapiInf(){
     const jsdata2 = await jsondata2.json();
     const dataforchart = jsdata2.statewise;
    
+    dataforchart.shift();
+    const jsondata3 = await fetch('https://api.covid19india.org/v4/min/data.min.json');
+    const dataj = await jsondata3.json();
+
+    const statelength = Object.keys(dataj).length;
+
+    const statedeltadata = Object.values(dataj);
+
+    console.log(statedeltadata[7]);
+
+//   Swapping the testing data for delhi and dadra nagra haveli
+  var temp = statedeltadata[8];
+  statedeltadata[8] = statedeltadata[7]
+  statedeltadata[7]=temp;
+//   statedeltadata[8].delta7.tested= '0';
+
+//   the json data file has some glitchh so
+
     const lengthofdata = Object.keys(dataforchart).length;
     const table2 = document.getElementById('tableid');
     var i=0;
@@ -21,7 +39,9 @@ async function getcovidapiInf(){
             <td class="dataletterspacing">${dataforchart[i].active.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
             <td class="dataletterspacing"> <span class="delta-confirmed recovered">+${dataforchart[i].deltarecovered.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</span><br>${dataforchart[i].recovered.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
             <td class="dataletterspacing"> <span class="delta-confirmed deaths">+${dataforchart[i].deltadeaths.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</span><br>${dataforchart[i].deaths.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
-            <td class="dataletterspacing">${dataforchart[i].migratedother.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
+            <td class="dataletterspacing"> <span class="delta-confirmed vaccinated">+${statedeltadata[i].delta7.vaccinated.toLocaleString('en-IN')}</span><br>${statedeltadata[i].total.vaccinated.toLocaleString('en-IN')}</td>
+            <td class="dataletterspacing tests">${statedeltadata[i].total.tested.toLocaleString('en-IN')}</td>
+            <td class="dataletterspacing">${statedeltadata[i].meta.population.toLocaleString('en-IN')}</td>
         </tr>`
             table2.innerHTML += row;
           

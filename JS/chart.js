fccb28bb8eed8f5eb5070b7e0fb36f26d724eca8
Kisalaya Kishan <kislay.kishan007@gmail.com>
2021-05-15 19:20:26 +0530

    chart added

--- JS/chart.js
+++ JS/chart.js
@@ -12,6 +12,8 @@ async function getcoviddata(){
     var yaxis=[];
     var dailyrecoverd=[];
     var deaths=[];
+    var dailyactive = [];
+    var activedata,recovery,difference;
     var avg;
     var sum=0;
     var mort=0;
@@ -32,6 +34,10 @@ async function getcoviddata(){
         dailyrecoverd.push(graphdata.cases_time_series[i].dailyrecovered); 
         deaths.push(graphdata.cases_time_series[i].dailydeceased);      
         
+            activedata = parseInt(graphdata.cases_time_series[i].dailyconfirmed);
+        recovery= parseInt(graphdata.cases_time_series[i].dailyrecovered);
+        difference = activedata-recovery;
+        dailyactive.push(difference);
     
     }
 
@@ -289,6 +295,45 @@ var datavaccine;
                     // 'rgba(114, 194, 23, 1)',
                     // 'rgba(75, 192, 192, 1)',
                     'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }
+        ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    var ctx = document.getElementById('myChart6').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: xaxisdatadate,
+            datasets: [
+            {
+                label: 'Active',
+                data: dailyactive,
+                backgroundColor: [
+                    'rgba(0,123,255,0.2)',
+                   
+                ],
+                borderColor: [
+                    // 'rgba(255, 99, 132, 1)',
+
+                    // 'rgba(219, 85, 129,1)',
+                    // 'rgba(54, 162, 235, 1)',
+                    // 'rgba(255, 206, 86, 1)',
+                    // 'rgba(114, 194, 23, 1)',
+                    // 'rgba(75, 192, 192, 1)',
+                    'rgba(0,123,255,1)',
                     // 'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1

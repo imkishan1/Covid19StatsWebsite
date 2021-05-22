9ff91c214c6de875a736d42bcdf6dd490951fb86
Kisalaya Kishan <kislay.kishan007@gmail.com>
2021-05-22 17:16:40 +0530

    Update cowinapi.js

--- JS/cowinapi.js
+++ JS/cowinapi.js
@@ -84,8 +84,5 @@ var date = document.getElementById('date').value;
 var pincode = document.getElementById("pin").value;
 
 
-{/* <div class="vaccinename">
-<p>${getdata.centers[i].sessions[0].vaccine}</p>
-</div> */}
 
 

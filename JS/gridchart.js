c121d14417b439553d23731a4cc3efbd571a4f44
Kisalaya Kishan <kislay.kishan007@gmail.com>
2021-06-05 10:24:01 +0530

    Update gridchart.js

--- JS/gridchart.js
+++ JS/gridchart.js
@@ -8,7 +8,7 @@ async function getcovidapiInf(){
     // const statedeltadata = Object.values(dataj);
     dataforchart.splice(31, 1);
     // console.log(dataforchart)
-    // console.log(dataj)
+    console.log(dataj)
     const lengthofdata = Object.keys(dataforchart).length;
     const table2 = document.getElementById('tableid');
     var i=0;
@@ -24,25 +24,29 @@ async function getcovidapiInf(){
           if(dataj[dataforchart[i].statecode].delta.tested==null)
           {
            
-            dataj[dataforchart[i].statecode].delta.tested="0";
+            dataj[dataforchart[i].statecode].delta.tested=0;
           }
           if(dataj[dataforchart[i].statecode].delta.confirmed==null)
           {
-            dataj[dataforchart[i].statecode].delta.confirmed="0";
+            dataj[dataforchart[i].statecode].delta.confirmed=0;
           }
           if(dataj[dataforchart[i].statecode].delta.recovered==null)
           {
-            dataj[dataforchart[i].statecode].delta.recovered="0";
+            dataj[dataforchart[i].statecode].delta.recovered=0;
           }
           if(dataj[dataforchart[i].statecode].delta.deceased==null)
           {
-            dataj[dataforchart[i].statecode].delta.deceased="0";
+            dataj[dataforchart[i].statecode].delta.deceased=0;
           }
-          if(dataj[dataforchart[i].statecode].delta.vaccinated==null)
+          if(dataj[dataforchart[i].statecode].delta.vaccinated1==null)
           {
-            dataj[dataforchart[i].statecode].delta.vaccinated="0";
+            dataj[dataforchart[i].statecode].delta.vaccinated1=0;
           }
-  
+          if(dataj[dataforchart[i].statecode].delta.vaccinated2==null)
+          {
+            dataj[dataforchart[i].statecode].delta.vaccinated2=0;
+          }
+            var totaldeltavcaccinated = parseInt(dataj[dataforchart[i].statecode].delta.vaccinated1)+parseInt(dataj[dataforchart[i].statecode].delta.vaccinated2)
             var totalvaccinated= parseInt(dataj[dataforchart[i].statecode].total.vaccinated1)+parseInt(dataj[dataforchart[i].statecode].total.vaccinated2)
           
             var row = `<tr class="tablerow">
@@ -51,9 +55,9 @@ async function getcovidapiInf(){
             <td class="dataletterspacing">${dataforchart[i].active.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
             <td class="dataletterspacing"> <span class="delta-confirmed recovered"><i class="fas fa-arrow-up"></i>${dataj[dataforchart[i].statecode].delta.recovered.toLocaleString('en-IN')}</span><br>${dataforchart[i].recovered.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
             <td class="dataletterspacing"> <span class="delta-confirmed deaths"><i class="fas fa-arrow-up"></i>${numDifferentiation(dataj[dataforchart[i].statecode].delta.deceased)}</span><br>${dataforchart[i].deaths.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
-            <td class="dataletterspacing"> ${numDifferentiation(dataj[dataforchart[i].statecode].total.vaccinated1)}</td>
-            <td class="dataletterspacing"> ${numDifferentiation(dataj[dataforchart[i].statecode].total.vaccinated2)}</td>
-            <td class="dataletterspacing"> ${numDifferentiation(totalvaccinated)}</td>
+            <td class="dataletterspacing"> <span class="delta-confirmed vaccinated"><i class="fas fa-arrow-up"></i>${numDifferentiation(dataj[dataforchart[i].statecode].delta.vaccinated1)}</span><br>${numDifferentiation(dataj[dataforchart[i].statecode].total.vaccinated1)}</td>
+            <td class="dataletterspacing"> <span class="delta-confirmed vaccinated"><i class="fas fa-arrow-up"></i>${numDifferentiation(dataj[dataforchart[i].statecode].delta.vaccinated2)}</span><br>${numDifferentiation(dataj[dataforchart[i].statecode].total.vaccinated2)}</td>
+            <td class="dataletterspacing"> <span class="delta-confirmed vaccinated"><i class="fas fa-arrow-up"></i>${numDifferentiation(totaldeltavcaccinated)}</span><br>${numDifferentiation(totalvaccinated)}</td>
             <td class="dataletterspacing"> <span class="delta-confirmed tests"><i class="fas fa-arrow-up"></i>${numDifferentiation(dataj[dataforchart[i].statecode].delta.tested)}</span><br>${numDifferentiation(dataj[dataforchart[i].statecode].total.tested)}</td>
             <td class="dataletterspacing">${numDifferentiation(dataj[dataforchart[i].statecode].meta.population)}</td>
                     </tr>`
@@ -92,6 +96,6 @@ function numDifferentiation (val) {
     return val;
   }
 
-  // <span class="delta-confirmed vaccinated"><i class="fas fa-arrow-up"></i>${numDifferentiation(dataj[dataforchart[i].statecode].delta.vaccinated)}</span><br></br>
+  // <span class="delta-confirmed vaccinated"><i class="fas fa-arrow-up"></i>${numDifferentiation(dataj[dataforchart[i].statecode].delta.vaccinated)}</span><br>
  // dataj[dataforchart[i].statecode].total.vaccinated
 

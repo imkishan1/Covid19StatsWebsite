5b28885d0b21e5f48fab4b70a53798b9d7603020
Kisalaya Kishan <kislay.kishan007@gmail.com>
2021-06-07 17:22:59 +0530

    Update time.js

--- JS/time.js
+++ JS/time.js
@@ -1,14 +1,43 @@
+// function currentTime(){
+//     var  date  =  new Date();
+//     var hour  = date.getHours();
+//     var min = date.getMinutes();
+//     hour = updateTime(hour);
+//     min = updateTime(min);
+//     var time = document.getElementById('time');
+//     var time1 = hour+":"+min;
+//     time.innerText = time1;
+//     var t = setTimeout(function(){ currentTime() }, 1000); 
+// }
+
+
 function currentTime(){
-    var  date  =  new Date();
-    var hour  = date.getHours();
-    var min = date.getMinutes();
-    hour = updateTime(hour);
-    min = updateTime(min);
-    var time = document.getElementById('time');
-    var time1 = hour+":"+min;
-    time.innerText = time1;
-    var t = setTimeout(function(){ currentTime() }, 1000); 
+  var  date  =  new Date();
+  var hour  = date.getHours();
+  var min = date.getMinutes();
+  // hour = updateTime(hour);
+  // min = updateTime(min);
+  var time = document.getElementById('time');
+  var amom = 'AM'
+  // var pm = 'pm'
+  if(hour>12)
+  {
+    hour = hour-12;
+    if(hour<10)
+    {
+      hour = "0"+hour;
+      am = 'PM'
+    }
+    else{
+      hour = hour-12;
+      am = 'PM'
+    }
+  }
+  var time1 = hour+":"+min+" "+am;
+  time.innerText = time1;
+  var t = setTimeout(function(){ currentTime() }, 1000); 
 }
+
 function updateTime(k) {
     if (k < 10) {
       return "0" + k;

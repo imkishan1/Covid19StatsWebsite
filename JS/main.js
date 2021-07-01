feb35774afde5aa018d0117c21ff30fb1d7317f2
Kisalaya Kishan <kislay.kishan007@gmail.com>
2021-07-01 09:17:51 +0530

    update

--- JS/main.js
+++ JS/main.js
@@ -5,37 +5,16 @@ async function getcovidapiIn(){
     const vaccinedata = Object.keys(jsdata.tested).length;
     var vaccinedatafinal = jsdata.tested[vaccinedata-1];
     const onedaybeforevaccine = jsdata.tested[vaccinedata-2];
-    // if(vaccinedatafinal.totaldosesadministered=='')
-    // {
-    //      vaccinedatafinal = jsdata.tested[vaccinedata-2];
-    //      onedaybeforevaccine = jsdata.tested[vaccinedata-3];
-    // }
+
     const size = Object.keys(jsdata.cases_time_series).length;
     const yesterdayupdate = jsdata.cases_time_series[size-1];
     const finaldata = jsdata.statewise[0];
-
-    // yesterday's log
-
-    // let yesterdaydate = document.querySelector('#yesterday-date');
-    // yesterdaydate.innerText = `${yesterdayupdate.date}`;
-
-    // let dailycnf = document.querySelector('#daily-conf');
-    // dailycnf.innerText = `${yesterdayupdate.dailyconfirmed.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
-
-    // let dailyrec = document.querySelector('#daily-rec');
-    // dailyrec.innerText = `${yesterdayupdate.dailyrecovered.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
-
-    // let dailydeath = document.querySelector('#daily-death');
-    // dailydeath.innerText = `${yesterdayupdate.dailydeceased.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
-
-    // let dailyupdatedon = document.querySelector('#date-time-last');
-    // dailyupdatedon.innerText = `${yesterdayupdate.dateymd}`;
 
     let todayvac = parseInt(vaccinedatafinal.totaldosesadministered)
     let deltavac = parseInt(onedaybeforevaccine.totaldosesadministered);
     let deltavaccine = (todayvac-deltavac).toString();
 
-    console.log(deltavaccine);
+    // console.log(deltavaccine);
     if(isNaN(deltavaccine))
     {
         deltavaccine='0';
@@ -53,32 +32,10 @@ async function getcovidapiIn(){
         vaccinedose.innerText = `${(vaccinedatafinal.totaldosesadministered).replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
     }
 
-   // yesterday's log
-
-    // let city = document.querySelector('#active-cases');
-    // city.innerText = `${finaldata.active.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
-
-    // const totalcase = document.querySelector('#total-cases');
-    // totalcase.innerText = `${finaldata.confirmed.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
-
-    // const totarecoverd = document.querySelector('#recovered');
-    // totarecoverd.innerText = `${finaldata.recovered.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
-
-    // const totaldeath = document.querySelector('#death');
-    // totaldeath.innerText = `${finaldata.deaths.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
-
-    // const newconfirm= document.querySelector('#new-confirm');
-    // newconfirm.innerText = `+${finaldata.deltaconfirmed.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
-
-    // const newrecovered= document.querySelector('#deltarec');
-    // newrecovered.innerText = `+${finaldata.deltarecovered.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
-    
-    // const newdeath= document.querySelector('#delta-death');
-    // newdeath.innerText = `+${finaldata.deltadeaths.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
 
     const lastupdate= document.querySelector('#date-time');
     lastupdate.innerText = `${finaldata.lastupdatedtime}`;
-    console.log(finaldata.lastupdatedtime);
+    // console.log(finaldata.lastupdatedtime);
 
     var  date  =  new Date();
     var datetoday = date.getDate();
@@ -106,7 +63,8 @@ async function getcovidapiIn(){
             finaltimeupdate = -(finaltimeupdate);
         }
     }
-    console.log(finaltimeupdate);
+    // console.log(finaltimeupdate);
+    lastupdate.innerText = `${finaltimeupdate}`;
 
 }
 
@@ -128,3 +86,52 @@ getcovidapiIn();
     // console.log(onedaybeforevaccine)
     // console.log(jsdata.tested)
     // console.log(yesterdayupdate)
+
+
+    // yesterday's log
+
+    // let yesterdaydate = document.querySelector('#yesterday-date');
+    // yesterdaydate.innerText = `${yesterdayupdate.date}`;
+
+    // let dailycnf = document.querySelector('#daily-conf');
+    // dailycnf.innerText = `${yesterdayupdate.dailyconfirmed.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
+
+    // let dailyrec = document.querySelector('#daily-rec');
+    // dailyrec.innerText = `${yesterdayupdate.dailyrecovered.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
+
+    // let dailydeath = document.querySelector('#daily-death');
+    // dailydeath.innerText = `${yesterdayupdate.dailydeceased.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
+
+    // let dailyupdatedon = document.querySelector('#date-time-last');
+    // dailyupdatedon.innerText = `${yesterdayupdate.dateymd}`;
+
+
+    
+   // yesterday's log
+
+    // let city = document.querySelector('#active-cases');
+    // city.innerText = `${finaldata.active.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
+
+    // const totalcase = document.querySelector('#total-cases');
+    // totalcase.innerText = `${finaldata.confirmed.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
+
+    // const totarecoverd = document.querySelector('#recovered');
+    // totarecoverd.innerText = `${finaldata.recovered.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
+
+    // const totaldeath = document.querySelector('#death');
+    // totaldeath.innerText = `${finaldata.deaths.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
+
+    // const newconfirm= document.querySelector('#new-confirm');
+    // newconfirm.innerText = `+${finaldata.deltaconfirmed.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
+
+    // const newrecovered= document.querySelector('#deltarec');
+    // newrecovered.innerText = `+${finaldata.deltarecovered.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
+    
+    // const newdeath= document.querySelector('#delta-death');
+    // newdeath.innerText = `+${finaldata.deltadeaths.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}`;
+
+        // if(vaccinedatafinal.totaldosesadministered=='')
+    // {
+    //      vaccinedatafinal = jsdata.tested[vaccinedata-2];
+    //      onedaybeforevaccine = jsdata.tested[vaccinedata-3];
+    // }

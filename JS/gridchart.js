97abd0577f7d451fc4f315a421d6dd0aae62a8f7
Kisalaya Kishan <kislay.kishan007@gmail.com>
2021-09-03 18:20:08 +0530

    Update gridchart.js

--- JS/gridchart.js
+++ JS/gridchart.js
@@ -170,10 +170,30 @@ const lastupdate= document.querySelector('#date-time');
             <td class="dataletterspacing classwidth"> <span class="delta-confirmed tests"><i class="fas fa-arrow-up"></i>${numDifferentiation(dataj[dataforchart[i].statecode].delta.tested)}</span>${numDifferentiation(dataj[dataforchart[i].statecode].total.tested)}</td>
             <td class="dataletterspacing classwidth-population">${numDifferentiation(dataj[dataforchart[i].statecode].meta.population)}</td>
                     </tr>`
-        
-                    table2.innerHTML += row;
+                      table2.innerHTML += row;
           }
-          else {
+          else { 
+           
+            if(dataj[dataforchart[i].statecode].delta.recovered<0 || dataj[dataforchart[i].statecode].delta.deceased<0 )
+            {
+              // var delta_recoverd_negative = ();
+              var row = `<tr class="tablerow">
+              <td class="fixedright color">${dataforchart[i].state}</td>
+              <td class="dataletterspacing" > <span class="delta-confirmed"><i class="fas fa-arrow-up"></i>${dataj[dataforchart[i].statecode].delta.confirmed.toLocaleString('en-IN')}</span>${dataforchart[i].confirmed.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
+              <td class="dataletterspacing classwidth-active">${dataforchart[i].active.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
+              <td class="dataletterspacing"> <span class="delta-confirmed recovered classwidth-recovered"><i class="fas fa-arrow-down"></i>${Math.abs(dataj[dataforchart[i].statecode].delta.recovered.toLocaleString('en-IN'))}</span>${dataj[dataforchart[i].statecode].total.recovered.toLocaleString('en-IN')}</td>
+              <td class="dataletterspacing classwidth-population"> <span class="delta-confirmed deaths"><i class="fas fa-arrow-down"></i>${numDifferentiation(Math.abs(dataj[dataforchart[i].statecode].delta.deceased))}</span>${dataj[dataforchart[i].statecode].total.deceased.toLocaleString('en-IN')}</td>
+              <td class="dataletterspacing">${numDifferentiation(dataj[dataforchart[i].statecode].total.vaccinated1)}</td>
+              <td class="dataletterspacing">${numDifferentiation(dataj[dataforchart[i].statecode].total.vaccinated2)}</td>
+              <td class="dataletterspacing">${numDifferentiation(totalvaccinated)}</td>
+              <td class="dataletterspacing classwidth"> <span class="delta-confirmed tests"><i class="fas fa-arrow-up"></i>${numDifferentiation(dataj[dataforchart[i].statecode].delta.tested)}</span>${numDifferentiation(dataj[dataforchart[i].statecode].total.tested)}</td>
+              <td class="dataletterspacing classwidth-population">${numDifferentiation(dataj[dataforchart[i].statecode].meta.population)}</td>
+                      </tr>`
+                      table2.innerHTML += row;
+            }
+            else{
+
+          
             var row = `<tr class="tablerow">
             <td class="fixedright color">${dataforchart[i].state}</td>
             <td class="dataletterspacing" > <span class="delta-confirmed"><i class="fas fa-arrow-up"></i>${dataj[dataforchart[i].statecode].delta.confirmed.toLocaleString('en-IN')}</span>${dataforchart[i].confirmed.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
@@ -186,9 +206,9 @@ const lastupdate= document.querySelector('#date-time');
             <td class="dataletterspacing classwidth"> <span class="delta-confirmed tests"><i class="fas fa-arrow-up"></i>${numDifferentiation(dataj[dataforchart[i].statecode].delta.tested)}</span>${numDifferentiation(dataj[dataforchart[i].statecode].total.tested)}</td>
             <td class="dataletterspacing classwidth-population">${numDifferentiation(dataj[dataforchart[i].statecode].meta.population)}</td>
                     </tr>`
-        
                     table2.innerHTML += row;
           }
+        }
         }
         else{
           var totalvaccinated= parseInt(dataj[dataforchart[i].statecode].total.vaccinated1)+parseInt(dataj[dataforchart[i].statecode].total.vaccinated2)
@@ -204,18 +224,6 @@ const lastupdate= document.querySelector('#date-time');
             <td class="dataletterspacing tests classwidth">${numDifferentiation(dataj[dataforchart[i].statecode].total.tested)}</td>
             <td class="dataletterspacing classwidth-population">${numDifferentiation(dataj[dataforchart[i].statecode].meta.population)}</td>
         </tr>`
-    // var row = `<tr class="tablerow">
-    //         <td class="fixedright color">${dataforchart[i].state}</td>
-    //         <td class="dataletterspacing" > <span class="delta-confirmed"><i class="fas fa-arrow-up"></i>${dataforchart[i].deltaconfirmed.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</span><br>${dataforchart[i].confirmed.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
-    //         <td class="dataletterspacing">${dataforchart[i].active.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
-    //         <td class="dataletterspacing"> <span class="delta-confirmed recovered"><i class="fas fa-arrow-up"></i>${dataforchart[i].deltarecovered.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</span><br>${dataforchart[i].recovered.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
-    //         <td class="dataletterspacing"> <span class="delta-confirmed deaths"><i class="fas fa-arrow-up"></i>${dataforchart[i].deltadeaths.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</span><br>${dataforchart[i].deaths.replace(/(\d)(?=(\d\d)+\d$)/g, "$1,")}</td>
-    //         <td class="dataletterspacing"> ${numDifferentiation(dataj[dataforchart[i].statecode].total.vaccinated1)}</td>
-    //         <td class="dataletterspacing"> ${numDifferentiation(dataj[dataforchart[i].statecode].total.vaccinated2)}</td>
-    //         <td class="dataletterspacing">${numDifferentiation(totalvaccinated)}</td>
-    //         <td class="dataletterspacing tests">${numDifferentiation(dataj[dataforchart[i].statecode].total.tested)}</td>
-    //         <td class="dataletterspacing">${numDifferentiation(dataj[dataforchart[i].statecode].meta.population)}</td>
-    //     </tr>`
             table2.innerHTML += row;
         }
         
